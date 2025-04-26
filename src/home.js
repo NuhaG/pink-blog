5e1a720cc68396bc6d4ab6dfc82c8625df666281
Nuha <nuhaghojaria@gmail.com>
2025-04-26 21:43:57 +0530

    click events

--- src/home.js
+++ src/home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log("Hello World", e); {/* e is event object */}
+    }
+    const handleClick2 = (name, e) => {
+        console.log("Hello " + name,e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click</button> 
+            {/* don't use handleClick() as it would invoke the function even brfore clicking the button */}
+            <button onClick={(e)=>handleClick2("Nuha",e)}>Click Me</button> 
+            {/*Passing parameters is nec nut we do not want to invoke the function so we wrap it in an anonymous func*/}
         </div>
      );
 }
