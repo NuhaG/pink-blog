e3bee3e9e0e62fa6c6fa2a7284b5f0b6e6425c6d
Nuha <nuhaghojaria@gmail.com>
2025-04-26 20:31:15 +0530

    creating home and nav components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './home';
 
 function App() {
-  const title = 'Welcome';
-  const likes = 50;
-  const link = "http://www.google.com"
-
-
   return (
     <div className="App">
-     <div className="content">
-      <h1>{title}</h1>
-      <p>Liked {likes} times</p>
-      <p>{[1,2,3,4]}</p>
-      <a href={link}>Google Site</a>
+      <Navbar />
+      <div className="content">
+        <Home />
      </div>
     </div>
   );
