137863e1022b74eb18496714fe60ca7cb492646d
Nuha <nuhaghojaria@gmail.com>
2025-04-26 21:55:49 +0530

    Hooks

--- src/home.js
+++ src/home.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
+
 const Home = () => {
-    let name = 'Nuha'; {/*Not Reactive so hooks are used, not reactive means it won't re-render the varianble on the page after it updates*/}
+    const [name, setName] = useState('Neha'); {/*Hook: used to re-render on page, first val is initial val and its updated using the second func*/}
+    const [age,setAge] = useState(18);
+
     const handleClick = (e) => {
-        name = 'Update';
+        setName('Sam');
+        setAge(20);
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <p>{name}</p>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click</button>
         </div>
      );
