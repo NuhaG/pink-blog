63cc1b750c61836de393685f196d7bcbb77cf286
Nuha <nuhaghojaria@gmail.com>
2025-04-27 15:26:49 +0530

    abort and cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 
-// custom hooks: used for reusing hooks
-
 const useFetch = (url) => {
     const [data,setData] = useState(null);
     const [isPending,setPending] = useState(true);
     const [error,setError] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController(); {/*we use abort controller so that it stops fetching the data as soon as another link is cliked, like we don't want the home page content to continue to be fetched in the background when we call the newBlog page*/}
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, {signal: abortCont.signal})
             .then(res => {
                 if(!res.ok) {
                     throw Error('Could not fetch the data for that resource');
@@ -22,12 +22,18 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(err => {
+                if(err.name === 'AbortError'){
+                    console.log("Fetch aborted");
+                } else {
                 setPending(false);
                 setError(err.message);
+                }
             })
         }, 1000);
-    }, [url]); 
-    // using url as a prop and dependency so that it rerenders as soon as the fetch url changes
+
+        return () => abortCont.abort();
+        {/*above return func runs as soon as a link clicked, we use the signal to stop the fetch*/}
+    }, [url]);
 
     return {data,isPending,error}
 }
