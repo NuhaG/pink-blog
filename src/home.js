d14d127ceb42338433b877b4f15ce68d7a8d5108
Nuha <nuhaghojaria@gmail.com>
2025-04-27 13:07:38 +0530

    loading message

--- src/home.js
+++ src/home.js
@@ -3,24 +3,25 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs,setBlogs] = useState(null);
+    const [isPending,setPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
+                setPending(false);
             })
+        }, 1000);
     }, []); 
-    // you can't make this async as we cannot use await inside it, you can externalize the whole func make that asyn then use it in useEffect
-    // res is just the response object and inorder to get the data we use a func
-    // at the very beg it will give an error as blogs is set to null initially and is passed to the bloglist.js where it map is used, which cannot func on nulll
 
     return ( 
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
-            {/* Runs only when blogs is not null*/}
         </div>
      );
 }
